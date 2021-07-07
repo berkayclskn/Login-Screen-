c619e1ec099edff0cbf7bf85db0f32ca2e7eefa5
berkayclskn <berkaycaliskan@gmail.com>
2021-07-07 16:48:27 +0300

    Login Screen

---
+++ app/src/main/java/com/example/androidstaj/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.androidstaj;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
--- app/src/main/java/com/example/androidstaj/MainActivity.kt
+++ app/src/main/java/com/example/androidstaj/MainActivity.kt
@@ -6,56 +6,44 @@ import android.view.Gravity
 import android.view.View
 import android.widget.*
 
-class Main : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
+    val Gender = resources.getStringArray(R.array.Gender),
 
-abstract class Spinner : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    val spinner = findViewById <Spinner>(R.id.spinner)
 
-    abstract val spinner_sample: Spinner?
-    var gender = arrayOf("Male", "Female", "Other")
+    if (spinner != null) {
+        val adapter = ArrayAdapter(this,
+            android.R.layout.simple_spinner_item, Gender)
+        spinner.adapter = adapter
 
-    var spinner: Spinner? = null
-    var textView_msg: TextView? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-
-        spinner = this.spinner_sample
-        spinner!!.setOnItemSelectedListener(this)
-
-
-        val aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, gender)
-
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-        spinner!!.setAdapter(aa)
-
-    }
-
-    abstract fun setAdapter(aa: ArrayAdapter<String>)
-
-    abstract fun setOnItemSelectedListener(spinner: Spinner)
-
-    override fun onItemSelected(arg0: AdapterView<*>, arg1: View, position: Int, id: Long) {
-        textView_msg!!.text = "Selected : " + gender[position]
-    }
-
-    override fun onNothingSelected(arg0: AdapterView<*>) {
+        spinner.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>,
+                                        view: View, position: Int, id: Long) {
+                Toast.makeText(this@MainActivity,
+                    getString(R.string.item) + " " +
+                            "" + Gender[position], Toast.LENGTH_SHORT).show()
+            }
 
 
+
+
+
+}
+
+
+        class toast: AppCompatActivity() {
         fun toastMessage(view: View) {
             val messageEditText = findViewById<EditText>(R.id.messageEditText)
             val message = messageEditText.text.toString()
             var toast = Toast.makeText(this, message, Toast.LENGTH_LONG)
             toast.show()
         }
-    }
-}
-}
+        }
+
+
